667c3b57d07d023514ac2eb038ee979a29fb6a1f
michi (｡•̀ᴗ-)✧ <admjgptw@yandex.ru>
2024-03-09 15:17:41 +0300

    poor code for sync with pc

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     println!("Welcome to Neko Manifest Creator!");
 
     let args: Vec<String> = std::env::args().collect();
-    println!("Args: {:?}", args)
+    println!("----------------------------");
+    println!("Selected loader: {}", args[2]);
+    println!("Selected version: {}", args[3]);
+    println!("----------------------------");
 }
